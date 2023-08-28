e4966d231bcd2ad6348f78bd42591f61a69f1c52
Jhonny Iriarte <iriarte.condori.jhonny25@gamil.com>
2023-08-28 10:12:49 -0400

    portafoliov2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import PerfilPhoto from "./components/perfil";
 
 function App() {
