85814ff42f85c2963f0f9c176ddded8dc62b2578
Jhonny Iriarte <iriarte.condori.jhonny25@gamil.com>
2023-08-28 10:52:38 -0400

    portafoliov4

--- src/components/perfil.jsx
+++ src/components/perfil.jsx
@@ -8,7 +8,7 @@ const PerfilPhoto = ()=> {
     return(
         <section className="bg-cover w-full h-full" style={divStyle}>
             <div className="flex flex-col items-center p-20   ">
-                <img src="../public/perfil-photo.png" alt="this is perfil photo" className=" object-cover object-center  rounded-full  w-52 h-52  border-red-600 border-4"  />
+                <img src="/perfil-photo.png" alt="this is perfil photo" className=" object-cover object-center  rounded-full  w-52 h-52  border-red-600 border-4"  />
                 <div className="flex flex-col p-5 my-5 items-center bg-slate-800 bg-opacity-25 rounded-lg">
                   <h1 className=" sm:text-4xl text-3xl text-white font-bold m-4">Jhonny Iriarte Condori</h1>
                   <h2 className=" text-cyan-400 text-2xl font-semibold">Sofware Ingineer</h2>
--- src/data.jsx
+++ src/data.jsx
@@ -3,15 +3,15 @@ export const Aplications = [
         title : "React Reserve",
         subtitle: " ecommerce ",
         description: "this is a project develop with React html, css , javascript ",
-        image: " public/projectLila.jpg ",
+        image: " /projectLila.jpg ",
         link:"https://ecomerce.netlify.app"
     },
 
     {
-        title : "calculator pro",
+        title : "calcultor pro",
         subtitle: " calculator ",
         description: "this is a project develop with React html, css , javascript ",
-        image: "public/projectAzul.jpg",
+        image: "/projectAzul.jpg",
         link:"https://ecomerce.netlify.app"
     },
 
@@ -19,7 +19,7 @@ export const Aplications = [
         title : "api rest",
         subtitle: " apirest",
         description: "this is a project develop with React html, css , javascript ",
-        image: "public/projectRojo.jpg",
+        image: "/projectRojo.jpg",
         link:"https://ecomerce.netlify.app"
     },
     
@@ -27,7 +27,7 @@ export const Aplications = [
         title : "api rest",
         subtitle: " apirest",
         description: "this is a project develop with React html, css , javascript ",
-        image: "public/perfil6.jpg",
+        image: "/perfil6.jpg",
         link:"https://ecomerce.netlify.app"
     }
 
@@ -58,7 +58,7 @@ export const Opiniones = [
     {
         id:1,
         quote : "  For commitment and diligence in delivering the assigned tasks remotely on Pfsense and Zabbix technologies.",
-        image: " /public/nerds.png",
+        image: " /nerds.png",
         name: "David Redecop",
         company: "Nerds onsite",
     },
@@ -66,7 +66,7 @@ export const Opiniones = [
     {
         id:2,
         quote : " For succesfully completing the foundattions in HTML, CSS, and Javascript module.",
-        image: " /public/academlo.png",
+        image: " /academlo.png",
         name: "Georg Berger",
         company: "Academlo",
     }
