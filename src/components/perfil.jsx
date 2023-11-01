f39e7a3e3817ed20bb9323647515b98cf61df6ed
Jhonny Iriarte Condori <84807709+Iriarte-star@users.noreply.github.com>
2023-10-31 21:18:51 -0400

    Update perfil.jsx

--- src/components/perfil.jsx
+++ src/components/perfil.jsx
@@ -14,8 +14,8 @@ const PerfilPhoto = ( {isEnglish} )=> {
                 <img src="/tiktok1.png" alt="this is perfil photo" className=" object-cover object-center   border-green-400  rounded-xl w-52 h-52   border-4"  />
                 <div className="flex flex-col p-5 my-5 items-center bg-slate-800 bg-opacity-50 rounded-lg">
                   <h1 className=" sm:text-4xl text-3xl text-white font-bold m-4">Jhonny Iriarte Condori</h1>
-                  <h2 className={`text-cyan-400 text-2xl font-semibold ${isEnglish ? "block" : "hidden"}`}>Sofware Ingineer</h2>
-                  <h2 className={`text-cyan-400 text-2xl font-semibold ${isEnglish ? "hidden" : "block"}`}>Ingeniero de sofware</h2>
+                  <h2 className={`text-cyan-400 text-2xl font-semibold ${isEnglish ? "block" : "hidden"}`}>Front End Developer</h2>
+                  <h2 className={`text-cyan-400 text-2xl font-semibold ${isEnglish ? "hidden" : "block"}`}>Desarrollador Frontend</h2>
                   <div className="flex gap-4 m-4 just "> 
                     <a href="https://github.com/Iriarte-star">
                       <FaGithub className="w-10 h-10 text-green-400"/>
