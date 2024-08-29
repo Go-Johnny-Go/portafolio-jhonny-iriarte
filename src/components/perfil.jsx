8260003c11638e789e2848293bf5b8604f6fd77d
Jhonny Iriarte Condori <84807709+Go-Johnny-Go@users.noreply.github.com>
2024-08-29 11:01:10 -0400

    Update perfil.jsx
    Modificación de cargo.

--- src/components/perfil.jsx
+++ src/components/perfil.jsx
@@ -12,9 +12,9 @@ const PerfilPhoto = ( {isEnglish} )=> {
         <section className="bg-cover w-full h-full" style={divStyle}>
             <div className="flex flex-col items-center p-20   ">
                 <div className="flex flex-col p-5 my-5 items-center bg-slate-800 bg-opacity-50 rounded-lg">
-                  <h2 className={`text-cyan-400 text-center text-3xl md:text-4xl font-new font-bold ${isEnglish ? "block" : "hidden"}`}>Front-End Developer</h2>
+                  <h2 className={`text-cyan-400 text-center text-3xl md:text-4xl font-new font-bold ${isEnglish ? "block" : "hidden"}`}>Software Engineer</h2>
                   <h1 className="  text-sm text-center md:text-3xl text-white font-bold m-4 hidden">Jhonny Iriarte Condori</h1>
-                  <h2 className={`text-cyan-400 text-3xl md:text-4xl font-new font-bold text-center ${isEnglish ? "hidden" : "block"}`}>Desarrollador Frontend</h2>
+                  <h2 className={`text-cyan-400 text-3xl md:text-4xl font-new font-bold text-center ${isEnglish ? "hidden" : "block"}`}>Ingeniero De Software</h2>
                   <div className="flex gap-4 m-4 just "> 
                     <a href="https://github.com/Iriarte-star" target="blank" >
                       <FaGithub className="w-10 h-10 text-green-400"/>
